58ea79acd2ecfe6cc87a3b9ed40b13d6ac5ee399
王野未 <826036140@qq.com>
2023-09-10 20:20:41 +0800

    refactor: remove jsx-plugin, remove typure-cli, change project version. etc.

--- typure-plugins/babel-jsx-plugin/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as BabelCore from '@babel/core'
-import syntaxJsx from '@babel/plugin-syntax-jsx'
-import transformTypureJsx from './transform-typure-jsx'
-export default ({ types }: typeof BabelCore) => ({
-  name: 'babel-plugin-jsx',
-  inherits: syntaxJsx,
-  ...transformTypureJsx
-})
--- typure-plugins/babel-jsx-plugin/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { BabelFile, types as t } from '@babel/core'
-
-export type State = {
-  get: (name: string) => any
-  set: (name: string, value: any) => any
-  file: BabelFile,
-  createElementIdentifier: string;
-}
